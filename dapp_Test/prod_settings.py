25e528857567ee9c2f2282b7a2c4126f75a2e824
nedimitri <nedimitri@gmail.com>
2018-09-28 21:46:55 +0100

    creating env variable for prod settings

---
+++ dapp_Test/prod_settings.py
@@ -0,0 +1,10 @@
+from dapp_Test.settings import *
+
+DEBUG = False
+
+TEMPLATE_DEBUG = False
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '@v-a-7j*@+b6&p$)dwh*fcth%#5x5v-b6q6x^sj42f1j9w_a4'
+
+ALLOWED_HOSTS = ['dapptest.herokuapp.com']
